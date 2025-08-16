2903139f995e2d917a3d22275d0bb3723db1a09c
Buckc95 <cb92845@gmail.com>
2025-08-15 19:07:34 -0500

    Eventos
    Almacena y guarda tareas y proyectos futuros

---
+++ EventosIbero/nginx/static/script.js
@@ -0,0 +1,17 @@
+async function login(){
+
+    let usuario = document.getElementById("usuario").value;
+    let clave = document.getElementById("clave").value;
+    
+    const resp = await fetch('/auth/login?usuario='+encodeURIComponent(usuario)+'&clave='+encodeURIComponent(clave),{
+        method: 'POST'
+    })
+
+    const data = await resp.json()
+
+    if(data.estado =="exitoso"){
+        window.location.href="eventos.html"
+    }else{
+        document.getElementById("mensajeLogin").innerText="Usuario o clave incorrectos"
+    }
+}
---
+++ EventosIbero/nginx/static/script_eventos.js
@@ -0,0 +1,54 @@
+async function crearEventos(){
+    let nombre = document.getElementById("nombre").value;
+    let fecha = document.getElementById("fecha").value;
+    let lugar = document.getElementById("lugar").value;  
+
+    await fetch(`/events/eventos?nombre=${encodeURIComponent(nombre)}&fecha=${fecha}&lugar=${encodeURIComponent(lugar)}`,{
+        method: 'POST'
+    })
+
+    cargarEventos();
+}
+
+async function editarEventos(id) {
+    const nuevoNombre = prompt("Nuevo nombre del evento: ")
+    const nuevaFecha = prompt("Nueva fecha (YYYY-MM-DD): ")
+    const nuevoLugar = prompt("Nuevo lugar del evento: ")
+    nuevoNombre
+    await fetch(`/events/eventos/${id}?nombre=${encodeURIComponent(nuevoNombre)}&fecha=${nuevaFecha}&lugar=${encodeURIComponent(nuevoLugar)}`,{
+        method: 'PUT'
+    })
+
+    cargarEventos()
+}
+
+async function eliminarEvento(id){
+    if(confirm("Estas seguro que quieres eliminar este evento")){
+        await fetch(`/events/eventos/${id}`,{
+            method: 'DELETE'
+        })
+        cargarEventos()
+    }
+}
+
+function cerrarSesion(){
+    window.location.href = "index.html"
+}
+
+async function cargarEventos(){
+    const resp= await fetch('/events/eventos')
+    const eventos = await resp.json()
+    let divEventos=document.getElementById("eventos")
+    divEventos.innerHTML = ""
+    eventos.forEach(ev => {
+        divEventos.innerHTML += `
+        <div class="evento">
+        <strong>${ev.nombre}</strong><br>
+        ${ev.fecha} - ${ev.lugar}<br>
+        <button onclick="editarEventos('${ev._id}')">Editar</button>
+        <button onclick="eliminarEvento('${ev._id}')" style="background:red;"> Eliminar</button>
+        </div>`     
+    });
+}
+
+cargarEventos()
